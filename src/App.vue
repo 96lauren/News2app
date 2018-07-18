100573b9e3907488361c166391523f3a37811627
Emmalauren sambu <eparlaurensanbu@gmail.com>
2018-07-18 11:48:22 +0300

    made app versatile

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
   
   <div class="container">
   	<h1>Top trending news WorldWide </h1>
+    <input v-model="search" type="search " name="search" placeholder=" type to search">
+    <br>
+    <button v-on:click="loadNews" type="button">search</button>
+    <p  v-if="loading"> please wait...</p>
+    
     <div v-for="article in news">
   <img :src="article.urlToImage" alt="">
   
@@ -20,10 +25,14 @@
 <script>
 
 import axios from 'axios'
+const baseurl ="https://newsapi.org/v2";
+const apikey ="49bc9bb4ad4b41fc93bdea885a96c962";
+const endpoint="/everything";
 
-const url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=49bc9bb4ad4b41fc93bdea885a96c962";
 const data = {
-news:[ ]
+news:[ ],
+search:'',
+loading:false 
    
 }
 
@@ -39,9 +48,19 @@ export default {
 
   methods:{
   	loadNews(){
+      if(this.search.length<1){
+        return;
+      }
+      this.loading= true;
+
+      this.news =[ ];
+
+      let url = baseurl + endpoint + '?q=' +this.search + '&apikey='+apikey;
+
       axios.get(url).then(function(response) {
  	    console.log(response.data.articles)
-          data.news= response.data.articles
+      data.loading = false;
+      data.news= response.data.articles
   
   }).catch(function(error){
   		  console.log(error.message)
